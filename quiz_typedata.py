ec0aa7475224de88411c7b7c136398701ca3d277
AlfauziStay <44737410+afauzi949@users.noreply.github.com>
2024-01-10 01:09:06 +0700

    quiz_typedata.py

---
+++ quiz_typedata.py
@@ -0,0 +1,38 @@
+"""
+TODO:
+ 1. Buatlah variabel startTime bertipe data dictionary sebagai waktu datang dengan kriteria berikut.
+    - key "HH" bernilai angka 9
+    - key "mm" bernilai angka 23
+    - key "ss" bernilai angka 57
+ 2. Buatlah variabel endTime:dict[str,tuple[str,int]] = {
+ "HH" : 15,
+ "mm" : 25,
+ "ss" : 43
+}print(bertipe data dictionary sebagai waktu pulang dengan kriteria berikut.
+    - key "HH" bernilai angka 15
+    - key "mm" bernilai angka 25
+    - key "ss" bernilai angka 43
+"""
+
+# Tulis kode Anda di bawah ini
+startTime:dict[str,int] = {"HH" : 9,"mm" : 23,"ss" : 57}
+endTime:dict[str,int] = {"HH" : 15,"mm" : 25,"ss" : 43}
+  
+print(startTime)
+print(endTime)
+
+
+"""
+TODO:
+ 1. Buatlah variabel inspectionList bertipe data list untuk menyimpan daftar inspeksi dengan isian sebagai berikut.
+    - pemahaman terhadap rencana dan desain
+    - ketersediaan bahan dan peralatan
+    - pengawasan pekerjaan
+    - kualitas konstruksi
+    - keselamatan kerja
+    - koordinasi dengan pemilik proyek
+"""
+
+# Tulis kode Anda di bawah ini
+inspectionList:list = ["pemahaman terhadap rencana dan desain","ketersediaan bahan dan peralatan","pengawasan pekerjaan","kualitas konstruksi","keselamatan kerja","koordinasi dengan pemilik proyek"]
+print(inspectionList)
